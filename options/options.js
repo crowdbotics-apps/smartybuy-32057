396947e1391a2585335dd3135178af4893493d17
Crowdbotics <team@crowdbotics.com>
2021-11-20 17:25:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "smartybuy_32057",
+  url: "https://smartybuy_32057.botics.co",
+  api: "https://smartybuy_32057.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
